7f08cbc34521d526c2ac1f26c9d1e49cb44410f0
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-09 12:06:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const newArray = string.split(' ').map(word => {
+        if (word != "the" && word != "a" && word != "an" && word != "but" && word != "of" && word != "and" && word != "for" && word != "at" && word != "by" && word != "from" ) {
+        return this.capitalize(word)
+      } else if ( string.indexOf(word) == 0 ) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    })
+    return newArray.join(' ')
+  }
+
 }
